a48d3bdb41dc7ce2c0e706c0b8abd25766dce41a
Aleksandr Karpov <papadoster13@gmail.com>
2023-02-22 21:43:23 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Александр Карпов on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
